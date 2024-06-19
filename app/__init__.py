8b67a17cfdbeb9df439266f2a5e94f751809e9fb
Macario Moura <macario.moura@gmail.com>
2024-06-19 13:08:01 -0300

    rewrote init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from app.routes import users
+from app.database import db
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     app.register_blueprint(users.user_bp)
 
+    db.init_app(app)
+
     return app
+
+
 
 
