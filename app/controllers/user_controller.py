0bc16c0fe504a29a417e523a3ecdc0e390050ea9
Macario Moura <macario.moura@gmail.com>
2024-06-19 09:41:32 -0300

    added controllers

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,2 +1,34 @@
 from flask import Request
+from app.services.user_service import UserService 
+from app.models.new_user_dto import NewUserDto
+
+
+class UserController():
+    def __init__(self, request: Request, user_service: UserService):
+        self.request = request
+        self.service = user_service
+
+    def create_user(self):
+        new_user = NewUserDto(name=self.request.json['name'])
+
+        user = self.service.create_user(new_user)
+
+        return[vars(user)], 201
+    
+
+    def delete_user(self):
+        user = self.service.delete_user(id=self.request.json['id'])
+        return [vars(user)], 200
+    
+    def get_user(self):
+        user = self.service.get_user(id=self.request.json['id'])
+        return[vars(user)], 201
+    
+    def update_user(self):
+        new_user = UserService(location=self.request.json['location'], name=self.request.json['name'])
+        
+        user = self.service.update_user(new_user)
+
+        return[vars(user)], 201
+
 
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,24 +1,24 @@
-from . import User
-from . import NewUserDTO
+from app.models.user_entity import User
+from app.models.new_user_dto import NewUserDto
 
 
 class UserService:
     def __init__(self):
         pass
 
-    def create_user(self, new_user: NewUserDTO) -> User:
+    def create_user(self, new_user: NewUserDto) -> User:
         user = self.create_user(new_user)
         #add  database operations
         return user
 
-    def update_user(self, id):
+    def update_user(self, user: User) -> User:
         return #alter user name
     
-    def delete_user(self, id):
+    def delete_user(self, user: User) -> User:
         return #database deletion
 
 
-    def get_user(self, id):
+    def get_user(self, user: User) -> User:
         return #select * from User WHERE id=':id'
 
         
