d80eb4ee1ad35a45a9ad56006a4158257b233b79
Macario Moura <macario.moura@gmail.com>
2024-06-19 13:05:12 -0300

    create and delete queries

--- app/database/user_repository.py
+++ app/database/user_repository.py
@@ -1,10 +1,38 @@
 from datetime import date
 from app.models.user_entity import User
-from app.models.new_user_dto import NewUserDTO
+from app.models.new_user_dto import NewUserDto
 
 
 class UserRepository():
-    def __init__(self, db_conn = []):
+    def __init__(self, db_conn):
         self.db = db_conn
 
-    
+    def create_user(self, user: NewUserDto) -> User:
+        sql = """
+        INSERT INTO users (id, name, location)
+        VALUES (:id, :name, :location)
+        RETURNING id, name, location
+        """
+
+        cursor = self.db.cursor()
+        cursor.execute(sql, {'id': user.id, 'name': user.name, 'location': user.location})
+        self.db.commit()
+
+        res = cursor.fetchone()
+        created_user = User(id=res['id'], name=res['name'], locations=res['location'])
+        cursor.close()
+        return created_user
+
+    def delete_user(self, user_id: int) -> User:
+        sql = """
+        DELETE FROM users WHERE id=:id
+        """
+
+        cursor = self.db.cursor()
+        cursor.execute(sql, {'id': user_id})
+        self.db.commit()
+
+        res = cursor.fetchone()
+        deleted_user = User(id=res['id'], name=res['name'], location=res['location'])
+        cursor.close()
+        return deleted_user
