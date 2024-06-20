9b854627778e6281f875a15f53fd4d5864901b69
Macario Moura <macario.moura@gmail.com>
2024-06-20 14:39:53 -0300

    added curson in db.py

--- app/database/db.py
+++ app/database/db.py
@@ -3,10 +3,11 @@ import sqlite3
 schema_file = "/home/macario/user_app/app/database/schema.sql"
 
 conn = sqlite3.connect('user_app.db', check_same_thread=False)
+cursor = conn.cursor()
 
 with open(schema_file,'r') as f:
     schema = f.read()
 
-conn.execute(schema)
-conn.commit()
-conn.close()
+cursor.execute(schema)
+cursor.commit()
+cursor.close()
