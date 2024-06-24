038cd979fccc6f9e5e09eb8526c83e17d114cef4
Macario Moura <macario.moura@gmail.com>
2024-06-24 14:36:19 -0300

    issues on pyproject.toml

--- app/database/db.py
+++ app/database/db.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-schema_file = "/home/macario/user_app/app/database/schema.sql"
+schema_file = "$HOME/user_app/app/database/schema.sql"
 
 conn = sqlite3.connect('user_app.db', check_same_thread=False)
 cursor = conn.cursor()
