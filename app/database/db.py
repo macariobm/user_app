1958b1adb8e27d68040ce457d024820ac8c045e1
macariobm <136586854+macariobm@users.noreply.github.com>
2024-06-26 08:25:15 -0300

    Update db.py

--- app/database/db.py
+++ app/database/db.py
@@ -8,7 +8,7 @@ conn = psycopg2.connect(host='localhost',
                        database='user_app',
                        user=os.environ['POSTGRES_USER'],
                        password=os.environ['POSTGRES_PASSWORD'])
-cursor = conn.cursor()
+cursor = conn.cursor(cursor_factory=DictCursor)
 
 with open(schema_file,'r') as f:
     schema = f.read()
